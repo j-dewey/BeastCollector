bff41d04709fb8593e435a179e58ee942d5b2092
j-dewey <jaredewey9@gmail.com>
2024-07-22 22:44:42 -0400

    Finalized sprite updates

--- items.py
+++ items.py
@@ -2,6 +2,7 @@
     Here are the actual items. Items also include spell effects for obvious reasons
 '''
 
+from _time import FrameCounter
 from item import Item, ItemEffect, ItemEffectType
 from projectile import Projectile
 
@@ -16,7 +17,9 @@ class FireSpawner(Item):
             scale
         )
         hitbox = pg.Rect(0, 0, scale[0], scale[1])
-        self.fire = Projectile(sprite, hitbox, 0.0, [0.0, 0.0])
+        # lives for 0 so it ends when mouse click ends
+        # TODO: make this not suck
+        self.fire = Projectile(sprite, hitbox, 0.0, [0.0, 0.0], FrameCounter(0))
 
     def get_effect(self, mpos: list[float]) -> ItemEffect:
         return ItemEffect(ItemEffectType.SPAWN_PROJECTILE, (self.fire))
